88abffc124b4627d2463a56c0c472b2832d80511
Alexey <alexey@MacBook-Pro.local>
2021-11-26 15:21:41 +0300

    Добавлено меню и немного доработан UI

--- Button.py
+++ Button.py
@@ -1,7 +1,9 @@
+import pygame as pg
 from Text import *
 
+
 class Button():
-    def __init__(self, screen, color: tuple, pos, width: int, height: int):
+    def __init__(self, screen, bg_color: tuple, color: tuple, pos, width: int, height: int, size: int, phrase: str):
         """
         :param screen - экран, нужно для отрисовки объектов
         :param color - цвет кнопки
@@ -11,19 +13,34 @@ class Button():
         
         self.type = "button"
         self.screen = screen
+        self.bg_color = bg_color
         self.color = color
+        self.size = size
         self.pos = pos
         self.width, self.height = width, height
 
-    def create_text(self, color, pos, size: int, phrase: str) -> None:
+        self.text = Text(self.screen, color, pg.Vector2(), size, phrase)
+        
+        twidth = self.text.text.get_width()
+        theight = self.text.text.get_height()
+        tpos = pg.Vector2(self.pos.x + self.width/2 - twidth/2, self.pos.y + self.height/2 - theight/2)
+        self.text.pos = tpos
+
+    '''
+    def create_text(self, color, size: int, phrase: str) -> None:
         """
         Функция создает текст для кнопки
 
         :param color, pos, size, phrase - эти параметры требуется передать в конструктор класса Text
         """
-        self.text = Text(self.screen, color, pos, size, phrase)
+        self.text = Text(self.screen, color, pg.Vector2(), size, phrase)
+        
+        twidth = self.text.text.get_width()
+        theight = self.text.text.get_height()
+        tpos = pg.Vector2(self.pos.x + self.width/2 - twidth/2, self.pos.y + self.height/2 - theight/2)
+        self.text.pos = tpos
         #--> Надо не забыть, что эту функцию надо вызывать сразу после создания экземпляра класса "Button"
-
+    '''
     def on_click(self, function) -> None:
         """
         Функция проверяет нажатие на кнопку и выполняет функцию "function"
@@ -31,13 +48,21 @@ class Button():
 
         :param function - функция для исполнения
         """
-        pass
+        mouse_pos = pg.mouse.get_pos()
+        if self.pos.x < mouse_pos[0] < (self.pos.x + self.width):
+            if self.pos.y < mouse_pos[1] < (self.pos.y + self.height):
+                print('tyk') 
+                self.bg_color = (200, 0, 0)
+                return function # доделать эту фигню!!!
+        return function
 
     def draw(self) -> None:
         """
         Отрисовка самой кнопки и текста внутри неё
         """
-        pass
+        pg.draw.polygon(self.screen, self.bg_color, [[self.pos.x, self.pos.y], [self.pos.x + self.width, self.pos.y], 
+                                               [self.pos.x +self.width, self.pos.y + self.height], [self.pos.x, self.pos.y + self.height]])
+        self.text.draw()
 
 
 if __name__ == '__main__':
--- UI.py
+++ UI.py
@@ -1,3 +1,10 @@
+import pygame as pg
+from Button import *
+from Text import *
+
+
+BLACK = (0,0,0)
+
 class UI:
     def __init__(self, screen):
         """
@@ -7,14 +14,15 @@ class UI:
         self.screen =  screen
         self.UI_elements = []
 
-    def add_button(self, sscreen, color: tuple, pos, width: int, height: int) -> None:
+    def add_button(self, screen, bg_color: tuple, color: tuple, pos, width: int, height: int, size: int, phrase: str) -> None:
         """
         Добавляет экземпляр класса "Кнопка" в массив всех элементов
 
         :param screen, color, pos, width, height - требуются для создания экземпляра
         класса Button
         """
-        pass
+        self.UI_elements.append(Button(screen, bg_color, color, pos, width, height, size, phrase))
+
 
     def add_text(self, screen, color, pos, size: int, phrase: str) -> None:
         """
@@ -23,21 +31,28 @@ class UI:
         :param screen, color, pos, size, phrase - требуются для создания экземпляра
         класса Text
         """
-        pass
+        self.UI_elements.append(Text(screen, color, pos, size, phrase))
 
-    def update(self, events) -> None:
+    def update(self, function: bool) -> None:
         """
         Функция обрабатывает все события, нужно для вызва функции on_click()
 
         :param events - массив со всеми событиями, нужно для обработки нажатий
         """
-        pass
+        for event in pg.event.get():
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    for i in self.UI_elements:
+                        if i.type == "button":
+                            i.on_click(function) # доделать эту фигню!!!
+        return function
 
     def draw(self) -> None:
         """
         Функция вызывает функцию draw() у всез элементов из массива UI_elements
         """
-        pass
+        for i in self.UI_elements:
+            i.draw()
 
 
 if __name__ == '__main__':
