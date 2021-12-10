4e9ecada805899e43e1b3a93026324c6242c6747
DmitriyAnn <pythonworld5>
2021-12-10 14:58:37 +0300

    Написана часть AI

--- AI.py
+++ AI.py
@@ -1,9 +1,12 @@
 from Obstacles import *
-
+import random
 
 class AI:
-    def __init__(self):
-        self.paterns = []
+    def __init__(self, screen):
+        self.screen = screen
+        self.patterns = [[pg.Vector3(100, 300, 500), pg.Vector3(300, 300, 500), pg.Vector3(500, 300, 500), pg.Vector3(700, 300, 500)],
+                         [pg.Vector3(100, 300, 1100), pg.Vector3(300, 300, 900), pg.Vector3(500, 300, 700), pg.Vector3(700, 300, 500)],
+                         [pg.Vector3(100, 300, 900), pg.Vector3(250, 300, 700), pg.Vector3(400, 300, 500), pg.Vector3(550, 300, 700), pg.Vector3(700, 300, 900)]]
 
     def update(self):
         """
@@ -11,17 +14,23 @@ class AI:
         """
         pass
 
-    def spawn_enemies(self, patern: list) -> list[Enemy]:
+    def spawn_enemies(self) -> list[Enemy]:
         """
-        Возвращает массив из "вражеских кораблей" согласно данным из массива patern
+        Возвращает массив из "вражеских кораблей" согласно данным из массива pattern
         """
-        pass
+        enemies = []
+        for i in random.choice(self.patterns):
+            enemies.append(Enemy(self.screen, i, 'Enemy_model.txt', pg.Vector3(0, 0, -1), 0))
+        return enemies
 
-    def spawn_coins(self, patern: list) -> list[Coin]:
+    def spawn_coins(self) -> list[Coin]:
         """
-        Возвращает массив из "Монет" согласно данным из массива patern
+        Возвращает массив из "Монет" согласно данным из массива pattern
         """
-        pass
+        coins = []
+        x = random.randint(50, 750)
+        coins.append(Coin(self.screen, pg.Vector3(x, 300, 500), 'Coin_model.txt', pg.Vector3(0, 0, -1), 0))
+        return coins
 
 
 if __name__ == '__main__':
